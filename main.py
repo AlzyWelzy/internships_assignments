c20ae1a643d92a8b9b4fcb872d0ec82687e53d6f
AlzyWelzy <welzyalzy@gmail.com>
2023-04-23 11:22:29 +0530

    and it's done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from algorithm_1 import main as algorithm_1
-from algorithm_3 import main as algorithm_3
+from algorithm_2 import main as algorithm_2
 from random_data import main as random_data
 
 
@@ -8,13 +8,14 @@ class MainProgram:
         # Define program descriptions
         self.program_descriptions = {
             "1": "Algorithm 1: Calculates target variables",
-            "2": "Algorithm 3: LSTM for student academic performance evaluation",
+            "2": "Algorithm 2: LSTM for student academic performance evaluation",
             "3": "Random Data: Generates random student performance data and saves to CSV",
         }
 
     def get_program_choice(self):
         # Print program descriptions and prompt user to choose a program
-        print("Please choose a program to run:")
+        print("*" * 50)
+        print("Please choose a program to run or type 'n' to exit:")
         for key, value in self.program_descriptions.items():
             print(f"{key}: {value}")
         program_choice = input("> ")
@@ -25,16 +26,20 @@ class MainProgram:
         if program_choice == "1":
             algorithm_1()
         elif program_choice == "2":
-            algorithm_3()
+            algorithm_2()
         elif program_choice == "3":
             random_data()
+        elif program_choice == "n":
+            exit()
         else:
             print("Invalid choice. Please try again.")
+            self.run_program(self.get_program_choice())
 
     def run(self):
-        # Runs the program by getting user input and running selected program
-        program_choice = self.get_program_choice()
-        self.run_program(program_choice)
+        while True:
+            # Runs the program by getting user input and running selected program
+            program_choice = self.get_program_choice()
+            self.run_program(program_choice)
 
 
 if __name__ == "__main__":
